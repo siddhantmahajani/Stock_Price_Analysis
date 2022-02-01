69a6bd2f6b8e4df941f64300930b7c6b3911f4ec
Siddhant Mahajani <sidhantmahajani@gmail.com>
2022-02-01 12:55:24 +0530

    Updated code
    Added author and description to files.

--- Stock_Price_Analysis/app.py
+++ Stock_Price_Analysis/app.py
@@ -1,3 +1,10 @@
+"""
+Author: Siddhant Mahajani
+Program: app.py
+Description: Web application to perform stock analysis based on Ticker provided from tickers/data.csv
+Date: 1st Feb 2022
+"""
+
 import json
 import plotly
 from flask import Flask, request, render_template
